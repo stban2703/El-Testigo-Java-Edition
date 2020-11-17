4c2bd03fad564d0a65117532f3a94d4318b46203
stban2703 <s.t.ban1401@gmail.com>
2020-11-17 12:29:07 -0500

    Added enemy and star counter

---
+++ src/model/Enemy.java
@@ -0,0 +1,126 @@
+package model;
+
+import processing.core.PApplet;
+import processing.core.PImage;
+
+public class Enemy {
+	private float posX;
+	private float posY;
+	private float width;
+	private float height;
+	private String type;
+	private int damage;
+	private int directionX;
+	private int directionY;
+	private float speed;
+	private PImage enemyimage;
+	private PApplet app;
+	
+	public Enemy(float posX, float posY, float width, float height, String type, int directionX, int directionY, float speed, int damage, PImage enemyimage,
+			PApplet app) {
+		this.posX = posX;
+		this.posY = posY;
+		this.width = width;
+		this.height = height;
+		this.type = type;
+		this.damage = damage;
+		this.directionX = directionX;
+		this.directionY = directionY;
+		this.speed = speed;
+		this.enemyimage = enemyimage;
+		this.app = app;
+	}
+	
+	public void paint() {
+		app.image(this.enemyimage, this.posX, this.posY, this.width, this.height);
+	}
+	
+	public void moveX() {
+		this.posX += speed * directionX;
+	}
+	
+	public void moveY() {
+		this.posY += speed * directionY;
+	}
+
+	public float getPosX() {
+		return posX;
+	}
+
+	public void setPosX(float posX) {
+		this.posX = posX;
+	}
+
+	public float getPosY() {
+		return posY;
+	}
+
+	public void setPosY(float posY) {
+		this.posY = posY;
+	}
+
+	public float getWidth() {
+		return width;
+	}
+
+	public void setWidth(float width) {
+		this.width = width;
+	}
+
+	public float getHeight() {
+		return height;
+	}
+
+	public void setHeight(float height) {
+		this.height = height;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getDirectionX() {
+		return directionX;
+	}
+
+	public void setDirectionX(int directionX) {
+		this.directionX = directionX;
+	}
+
+	public int getDirectionY() {
+		return directionY;
+	}
+
+	public void setDirectionY(int directionY) {
+		this.directionY = directionY;
+	}
+
+	public float getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(float speed) {
+		this.speed = speed;
+	}
+
+	public int getDamage() {
+		return damage;
+	}
+
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+
+	public PImage getEnemyimage() {
+		return enemyimage;
+	}
+
+	public void setEnemyimage(PImage enemyimage) {
+		this.enemyimage = enemyimage;
+	}
+	
+}
--- src/view/Main.java
+++ src/view/Main.java
@@ -4,6 +4,7 @@ import view.Main;
 
 import java.util.ArrayList;
 
+import model.Enemy;
 import model.Player;
 import model.Star;
 import model.Tile;
@@ -19,6 +20,8 @@ public class Main extends PApplet {
 
 	Serial serial;
 
+	private int screen;
+	private int starsCount = 0;
 	private int rows = 11;
 	private int colums = 22;
 	private Tile tileArray[][] = new Tile[rows][colums];
@@ -33,16 +36,44 @@ public class Main extends PApplet {
 
 	private ArrayList<Star> starList = new ArrayList<Star>();
 	private PImage starImage;
+	
+	private ArrayList<Enemy> enemyList = new ArrayList<Enemy>();
+	
+	private int deathMatX;
+	private int deathMatY;
 
+	private int apathyMatX;
+	private int apathyMatY;
+
+	private int forgetMatX;
+	private int forgetMatY;
+
+	private int unkwonMatX;
+	private int unkwnMatY;
+
+	
+	private PImage deathImage;
+	private PImage apathyImage;
+	private PImage forgetImage;
+	private PImage unkwonImage;
+	
 	private PImage gameplayImage;
+
+	private PImage firstImage;
+	private PImage secondImage;
+	private PImage thirdImage;
+	private PImage fourthImage;
+	private PImage fifthImage;
 
 	public void settings() {
 		size(1200, 700);
 	}
 
 	public void setup() {
-		// logic = new Logic(this);
 		background(155);
+
+		screen = 0;
+		starsCount = 0;
 
 		String[] puertos = Serial.list();
 		printArray(puertos);
@@ -50,10 +81,36 @@ public class Main extends PApplet {
 		serial.bufferUntil('\n');
 
 		this.playerImage = loadImage("../img/player.png");
-		this.player = new Player(7 + tilezeroX + tileWidth * playerMatX, tilezeroY + tileWidth * playerMatY, 32, 45, 49,
+		this.player = new Player(7 + tilezeroX + tileWidth * playerMatX, 3 + tilezeroY + tileWidth * playerMatY, 32, 45, 49,
 				3, 0, this.playerImage, this);
 
 		this.starImage = loadImage("../img/star.png");
+		
+		deathImage = loadImage("../img/muerte.png");
+		apathyImage = loadImage("../img/apatia.png");
+		forgetImage = loadImage("../img/olvido.png");
+		unkwonImage = loadImage("../img/desconocimiento.png");
+		
+		deathMatX = 6;
+		deathMatY = 0;
+
+		apathyMatX = 7;
+		apathyMatY = 7;
+
+		forgetMatX = 7;
+		forgetMatY = 7;
+
+		unkwonMatX = 7;
+		unkwnMatY = 7;
+		
+		this.enemyList.add(new Enemy(tilezeroX + tileWidth * deathMatX, tilezeroY + tileWidth * deathMatY, 49, 49, "death",
+				1, 1, tileWidth, 3, deathImage, this));
+
+		this.firstImage = loadImage("../img/pantalla01.jpg");
+		this.secondImage = loadImage("../img/pantalla02.jpg");
+		this.thirdImage = loadImage("../img/pantalla03.jpg");
+		this.fourthImage = loadImage("../img/pantalla04.jpg");
+		this.fifthImage = loadImage("../img/pantalla05.jpg");
 		this.gameplayImage = loadImage("../img/mapa.jpg");
 
 		for (int i = 0; i < this.rows; i++) {
@@ -181,85 +238,215 @@ public class Main extends PApplet {
 	}
 
 	public void draw() {
-		// logic.paintScreen();
-		// logic.checkCollision();
-		for (int i = 0; i < this.rows; i++) {
-			for (int j = 0; j < this.colums; j++) {
-				this.tileArray[i][j].paint();
+
+		switch (screen) {
+		case 0:
+			image(this.firstImage, 0, 0, 1200, 700);
+			if(mouseX > 507 && mouseX < 507 + 186 && mouseY > 479 && mouseY < 479 + 52) {
+				cursor(HAND);
+			} else {
+				cursor(ARROW);
 			}
+			break;
+
+		case 1:
+			image(this.secondImage, 0, 0, 1200, 700);
+			if(mouseX > 907 && mouseX < 907 + 222 && mouseY > 571 && mouseY < 571 + 52) {
+				cursor(HAND);
+			} else {
+				cursor(ARROW);
+			}
+			break;
+
+		case 2:
+			image(this.thirdImage, 0, 0, 1200, 700);
+			if(mouseX > 907 && mouseX < 907 + 222 && mouseY > 571 && mouseY < 571 + 52) {
+				cursor(HAND);
+			} else {
+				cursor(ARROW);
+			}
+			break;
+
+		case 3:
+			image(this.fourthImage, 0, 0, 1200, 700);
+			if(mouseX > 907 && mouseX < 907 + 222 && mouseY > 571 && mouseY < 571 + 52) {
+				cursor(HAND);
+			} else {
+				cursor(ARROW);
+			}
+			break;
+
+		case 4:
+			image(this.fifthImage, 0, 0, 1200, 700);
+			if(mouseX > 600 && mouseX < 600 + 222 && mouseY > 582 && mouseY < 582 + 52) {
+				cursor(HAND);
+			} else {
+				cursor(ARROW);
+			}
+			break;
+
+		case 5:
+			cursor(ARROW);
+			
+			for (int i = 0; i < this.rows; i++) {
+				for (int j = 0; j < this.colums; j++) {
+					this.tileArray[i][j].paint();
+				}
+			}
+
+			image(this.gameplayImage, 0, 0, 1200, 700);
+			fill(255);
+			textSize(20);
+			text(starsCount, 895, 45);
+
+			this.player.paint();
+
+			// Generate star
+			if (frameCount % 40 == 0) {
+
+				int randomRow = (int) Math.floor(random(11));
+				int randomColum = (int) Math.floor(random(22));
+
+				Tile tile = tileArray[randomRow][randomColum];
+
+				if (tile.getType() == 1) {
+					starList.add(new Star(tile.getPosX() + 9, tile.getPosY() + 9, 30, 30, this.starImage, this));
+				}
+			}
+			
+			
+			// Paint star
+			for (int i = 0; i < starList.size(); i++) {
+				starList.get(i).paint();
+			}
+
+			float playerX = this.player.getPosX();
+			float playerY = this.player.getPosY();
+			float playerWidth = this.player.getWidth();
+			float playerHeight = this.player.getHeight();
+
+			// Get stars
+			for (int i = 0; i < this.starList.size(); i++) {
+				Star star = this.starList.get(i);
+				float starX = star.getPosX();
+				float starY = star.getPosY();
+
+				if (starX > playerX && starX < playerX + playerWidth && starY > playerY
+						&& starY < playerY + playerHeight) {
+					starList.remove(i);
+					serial.write("ledon\n");
+					starsCount++;
+				}
+
+			}
+			
+			for (int i = 0; i < this.enemyList.size(); i++) {
+				Enemy enemy = this.enemyList.get(i);
+				enemy.paint();
+			}	
+			
+			
+			// Collision enemies
+			for (int i = 0; i < this.enemyList.size(); i++) {
+				Enemy enemy = this.enemyList.get(i);
+				float enemyX = enemy.getPosX();
+				float enemyY = enemy.getPosY();
+				float enemyWidth = enemy.getWidth();
+				float enemyHeight = enemy.getHeight();
+				
+				if (playerX >= enemyX && playerX < enemyX + enemyWidth && playerY > enemyY
+						&& playerY < enemyY + enemyHeight) {
+					System.out.println("Colision");
+				}
+
+			}
+			break;
+
+		default:
+			break;
 		}
 
-		image(this.gameplayImage, 0, 0, 1200, 700);
+	}
 
-		this.player.paint();
-
-		if (frameCount % 60 == 0) {
-
-			int randomRow = (int) Math.floor(random(11));
-			int randomColum = (int) Math.floor(random(22));
-
-			Tile tile = tileArray[randomRow][randomColum];
-
-			if (tile.getType() == 1) {
-				starList.add(new Star(tile.getPosX() + 9, tile.getPosY() + 9, 30, 30, this.starImage, this));
+	public void mousePressed() {
+		switch (screen) {
+		case 0:
+			if(mouseX > 507 && mouseX < 507 + 186 && mouseY > 479 && mouseY < 479 + 52) {
+				screen = 1;
 			}
-		}
+			break;
 
-		for (int i = 0; i < starList.size(); i++) {
-			starList.get(i).paint();
-		}
-
-		float playerX = this.player.getPosX();
-		float playerY = this.player.getPosY();
-		float playerWidth = this.player.getWidth();
-		float playerHeight = this.player.getHeight();
-
-		for (int i = 0; i < this.starList.size(); i++) {
-			Star star = this.starList.get(i);
-			float starX = star.getPosX();
-			float starY = star.getPosY();
-
-			if (starX > playerX && starX < playerX + playerWidth && starY > playerY && starY < playerY + playerHeight) {
-				starList.remove(i);
-				serial.write("ledon\n");
+		case 1:
+			if(mouseX > 907 && mouseX < 907 + 222 && mouseY > 571 && mouseY < 571 + 52) {
+				screen = 2;
 			}
+			break;
 
+		case 2:
+			if(mouseX > 907 && mouseX < 907 + 222 && mouseY > 571 && mouseY < 571 + 52) {
+				screen = 3;
+			}
+			break;
+
+		case 3:
+			if(mouseX > 907 && mouseX < 907 + 222 && mouseY > 571 && mouseY < 571 + 52) {
+				screen = 4;
+			}
+			break;
+
+		case 4:
+			if(mouseX > 600 && mouseX < 600 + 222 && mouseY > 582 && mouseY < 582 + 52) {
+				screen = 5;
+			}
+			break;
+
+		case 5:
+
+			break;
+
+		default:
+			break;
 		}
 	}
 
 	public void keyPressed() {
-		// logic.keyEvents();
-		if (keyCode == UP) {
-			// if we aren't in the top row and the cell above us doesn't contain an obstacle
-			// then we can move up
-			if (player.getPosY() > 115 && tileArray[playerMatY - 1][playerMatX].getType() != 0) {
-				this.player.moveUp();
-				playerMatY--;
+
+		switch (screen) {
+		case 5:
+			// logic.keyEvents();
+			if (keyCode == UP) {
+				// if we aren't in the top row and the cell above us doesn't contain an obstacle
+				// then we can move up
+				if (player.getPosY() > 115 && tileArray[playerMatY - 1][playerMatX].getType() != 0) {
+					this.player.moveUp();
+					playerMatY--;
+				}
+			} else if (keyCode == DOWN) {
+				// if we aren't in the bottom row and the cell below us doesn't contain an
+				// obstacle
+				// then we can move down
+				if (player.getPosY() < 115 + (490) && tileArray[playerMatY + 1][playerMatX].getType() != 0) {
+					this.player.moveDown();
+					playerMatY++;
+				}
+			} else if (keyCode == LEFT) {
+				// if we aren't in the left-most column and the cell to our left doesn't contain
+				// an obstacle
+				// then we can move left
+				if (player.getPosX() > 59 + 7 && tileArray[playerMatY][playerMatX - 1].getType() != 0) {
+					this.player.moveLeft();
+					playerMatX--;
+				}
+			} else if (keyCode == RIGHT) {
+				// if we aren't in the right-most column and the cell to our right doesn't
+				// contain an obstacle
+				// then we can move right
+				if (player.getPosX() < 59 + 1029 && tileArray[playerMatY][playerMatX + 1].getType() != 0) {
+					this.player.moveRight();
+					playerMatX++;
+				}
 			}
-		} else if (keyCode == DOWN) {
-			// if we aren't in the bottom row and the cell below us doesn't contain an
-			// obstacle
-			// then we can move down
-			if (player.getPosY() < 115 + (490) && tileArray[playerMatY + 1][playerMatX].getType() != 0) {
-				this.player.moveDown();
-				playerMatY++;
-			}
-		} else if (keyCode == LEFT) {
-			// if we aren't in the left-most column and the cell to our left doesn't contain
-			// an obstacle
-			// then we can move left
-			if (player.getPosX() > 59 + 7 && tileArray[playerMatY][playerMatX - 1].getType() != 0) {
-				this.player.moveLeft();
-				playerMatX--;
-			}
-		} else if (keyCode == RIGHT) {
-			// if we aren't in the right-most column and the cell to our right doesn't
-			// contain an obstacle
-			// then we can move right
-			if (player.getPosX() < 59 + 1029 && tileArray[playerMatY][playerMatX + 1].getType() != 0) {
-				this.player.moveRight();
-				playerMatX++;
-			}
+			break;
 		}
 
 	}
